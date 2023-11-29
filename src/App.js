4ff6213eb7df9d39d64754681653648dd60e891b
macmost935 <48839894+macmost935@users.noreply.github.com>
2023-11-29 21:53:22 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-/**
- * Uses Tailwind CSS for styling
- * Tailwind file is imported in App.css
- */
-
-export default function App() {
-  return (
-    <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
-      <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img
-          className="col-span-2 row-span-3 animate-spin m-auto"
-          style={{ animationDuration: "30s" }}
-          src={logo}
-          alt="React Logo"
-          width="300"
-        />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-      </div>
-
-      <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Welcome to Martyns Law{" "}
-        <span className="block text-lg text-blue-400">on DigitalOcean</span>
-      </h1>
-
-      <div className="grid grid-cols-2 grid-rows-2 gap-4">
-        <Button
-          NI
-          text="Moyo Yavo Yakaoma Zviri Nani Ndigare Kure Nehama"
-          url="https://www.youtube.com/watch?v=5lvB3FCtxsk&list=TLPQMTExMTIwMjNdv9UAw3ysGA&index=3"
-        />
-        <Button
-          text="Yahoo"
-          url="https://yahoo.com"
-        />
-      </div>
-    </div>
-  );
-}
-
-function Button({ className, text, url = "#" }) {
-  return (
-    <a
-      href={url}
-      className={`${className} py-3 px-6 bg-purple-400 hover:bg-purple-300 text-purple-800 hover:text-purple-900 block rounded text-center shadow flex items-center justify-center leading-snug text-xs transition ease-in duration-150`}
-    >
-      {text}
-    </a>
-  );
-}
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const schema = require("./schemas/studentCourseSurvey");
+ 
+const app = express();
+const PORT = process.env.PORT;
+ 
+//CSRF setup
+const csrfProtect = csrf({
+  cookie: true,
+});
+ 
+// Middleware
+app.use(express.json()); // Parse JSON bodies for this app
+app.use(cors({ // use CORS
+credentials: true, 
+origin: process.env.allowed_origins
+})); 
+app.use(cookieParser()); // cookieparser, required for csurf
+app.use(csrfProtect); // use CSRF protection
+ 
+// Route for CSRF token
+app.get("/getCSRFToken", function (req, res) {
+  res.json({ CSRFToken: req.csrfToken() });
+});
+ 
+// GET route for sending schema
+app.get("/api", function (req, res) {
+  res.json(schema);
+});
+ 
+// POST route
+app.post("/api", async (req, res) => {
+  // req.body is our survey response; send this back to your real database here
+  res.send(req.body);
+});
+ 
+app.listen(PORT, () =>
+  console.log(`Hello world app listening on port ${PORT}!`)
+);
